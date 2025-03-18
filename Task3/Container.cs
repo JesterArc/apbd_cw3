cf3cabe39920e159bac115b96d72d4590e101512
JesterArc <125870978+JesterArc@users.noreply.github.com>
2025-03-18 12:59:21 +0100

    Base class Container added

---
+++ Task3/Container.cs
@@ -0,0 +1,44 @@
+﻿namespace Task3;
+
+public class OverfillException : Exception
+{
+    public OverfillException() : base("Maximum payload size exceeded.")
+    {
+        
+    }
+}
+
+public class Container
+{
+    protected double CurrentPayload;
+    protected double Height;
+    protected double Weight;
+    protected double Depth;
+    protected char ContainerType;
+    protected Guid ContainerId;
+    protected string SeriesNumber;
+    protected readonly double MaximumPayload;
+
+    public Container(double height, double weight, double depth, char containerType, double maximumPayload)
+    {
+        Height = height;
+        Weight = weight;
+        Depth = depth;
+        MaximumPayload = maximumPayload;
+        ContainerId = Guid.NewGuid();
+        SeriesNumber = $"KON-${containerType}-${ContainerId}";
+    }
+    public void Unload()
+    {
+        CurrentPayload = 0;
+    }
+
+    public void Load(double payload)
+    {
+        if (payload > MaximumPayload - CurrentPayload)
+        {
+            throw new OverfillException();
+        }
+        CurrentPayload += payload;
+    }
+}
